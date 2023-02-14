d44bbe3e9e10f7e559b74f1d7c5fb9bc1321a223
Blessed Sibanda <blessedsibanda.django@gmail.com>
2023-02-14 14:46:28 +0200

    setup turbo-drive

--- frontend/src/application/turbo_drive.js
+++ frontend/src/application/turbo_drive.js
@@ -1,5 +1,11 @@
 import "../styles/turbo_drive.scss";
 
+import "@hotwired/turbo";
+
 window.document.addEventListener("DOMContentLoaded", function () {
-  window.console.log("dom ready 1");
+    window.console.log("dom ready 1");
+});
+
+document.addEventListener("turbo:load", function () {
+    console.log("turbo:load");
 });
